e424e541c121c1d53056e33f78b7d255db6f7347
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2019-12-31 23:27:35 -0800

    Support recursive templates

--- template.js
+++ template.js
@@ -1,56 +1,49 @@
-#!/usr/bin/env node
-
 let fs = require("fs");
 let path = require("path");
 
 // Parse arguments
 let args = process.argv.slice(2);
-if (!Array.isArray(args) || args.length != 2) {
-    throw "usage: template [input] [output]";
+if (!Array.isArray(args) || args.length != 2) { 
+    throw "usage: template [input] [output]"; 
 }
-let inputPath = args[0];
-let outputPath = args[1];
+let inputPath = args[0], outputPath = args[1];
+let output = compile(inputPath);
+try { fs.writeFileSync(outputPath, output); } 
+catch (error) { throw "unable to write to file " + outputPath; }
 
-// Read input
-let blueprint = "";
-try {
-    blueprint = fs.readFileSync(inputPath).toString();
-} catch (error) {
-    throw "unable to read from file " + inputPath;
-}
+function compile(inputPath) {
+    // Read input
+    let input = "";
+    try { input = fs.readFileSync(inputPath).toString(); } 
+    catch (error) { throw "unable to read from file " + inputPath; }
 
-// Find templates
-let start = -1, end = -1;
-while ((start = blueprint.indexOf("[", end)) != -1 && (end = blueprint.indexOf("]", start)) != -1) {
-    // Check for escapes
-    if (start > 0 && blueprint[start - 1] == '\\') {
-        blueprint = blueprint.slice(0, start - 1) + blueprint.slice(start)
-        continue;
-    }
-    // Check if a file path
-    let template = path.join(path.dirname(path.resolve(inputPath)), blueprint.substring(start + 1, end));
-    if (!fs.existsSync(template)) {
-        continue;
-    }
-    // Replace template
-    try {
-        let extension = template.split(".");
-        extension = extension[extension.length - 1];
-        if (extension == "js") {
-            let js = require(template);
-            blueprint = blueprint.substring(0, start) + js.main(path.dirname(template) + "/") + blueprint.substring(end + 1);
-        } else if (extension == "html") {
-            blueprint = blueprint.substring(0, start) + fs.readFileSync(template) + blueprint.substring(end + 1);
+    // Find templates
+    let start = -1, end = -1;
+    while ((start = input.indexOf("[", end)) != -1 && (end = input.indexOf("]", start)) != -1) {
+        // Check for escapes
+        if (start > 0 && input[start - 1] == '\\') {
+            input = input.slice(0, start - 1) + input.slice(start)
+            continue;
+        }
+        // Check if a file path
+        let template = path.join(path.dirname(path.resolve(inputPath)), input.substring(start + 1, end));
+        if (!fs.existsSync(template)) {
+            continue;
+        }
+        // Replace template
+        try {
+            let extension = template.split(".");
+            extension = extension[extension.length - 1];
+            if (extension == "js") {
+                let js = require(template);
+                input = input.substring(0, start) + js.main(path.dirname(template) + "/") + input.substring(end + 1);
+            } else if (extension == "html") {
+                input = input.substring(0, start) + compile(template) + input.substring(end + 1);
+            }
+        }
+        catch (error) {
+            console.warn("unable to load or evaluate " + template + ':\n' + error);
         }
     }
-    catch (error) {
-        console.warn("unable to load or evaluate " + template + ':\n' + error);
-    }
-}
-
-// Write outut
-try {
-    fs.writeFileSync(outputPath, blueprint);
-} catch (error) {
-    throw "unable to write to file " + outputPath;
+    return input;
 }
